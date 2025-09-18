5f32495355bd51e3b132fa5f6bad97ce0062cbcd
bagdat <bagdat@remofirst.com>
2025-09-18 12:17:59 +0500

    classwork 4

--- classwork4/src/App.jsx
+++ classwork4/src/App.jsx
@@ -1,23 +1,42 @@
 import { useCallback, useRef, useState } from 'react';
-import TodoList from './components/TodoList/Todolist';
+import { TodoList } from './components/TodoList/Todolist';
 
 export default function App() {
 
-  const [list, setList] = useState([ 'First task', 'Second Task'])
-  
+  const [list, setList] = useState([
+    {id: 1, name: '1 todo'},
+    {id: 2, name: '2 todo'},
+  ])
+
   const inputRef = useRef(null);
+  const count = useRef(0);
 
   function handleFocus () {
     inputRef.current.focus();
   }
 
+
   function handleBlur () {
     inputRef.current.blur();
   }
 
-  const onAddNewTask = useCallback(() => {
-    setList([...list, `New task ${list.length + 1}`])
+  function handleSelect () {
+    inputRef.current.select();
+  }
+
+  const addCountAlert = useCallback(() => {
+    alert(`Count is: ${count.current}`)
+    count.current = count.current + 1;
+  }, [])
+
+  const handleAddTodo = useCallback((text) => {
+    setList([...list, {id: list.length + 1, name: text}])
   }, [list])
+
+
+  const handleEditTodo = (index, text) => {
+    setList(list.map((item,idx) => index===idx ? ({id: item.id, name: text}): item))
+  }
 
 
 
@@ -26,8 +45,11 @@ export default function App() {
     <input ref={inputRef} />
     <button onClick={handleFocus}>focus</button>
     <button onClick={handleBlur}>blur</button>
+    <button onClick={handleSelect}>select</button>
+    <button onClick={addCountAlert}>show alert count</button>
 
-    <TodoList list={list} handleAdd={onAddNewTask}/>
+    <TodoList list={list} handleAdd={handleAddTodo} handleEdit={handleEditTodo}/>
+
    </>
   );
 }
--- classwork4/src/components/TodoList/Todolist.jsx
+++ classwork4/src/components/TodoList/Todolist.jsx
@@ -1,14 +1,51 @@
-export default function TodoList ({list, handleAdd}) {
+import { memo, useRef, useState } from "react"
 
+export const TodoList = memo(({list, handleAdd, handleEdit}) => {
+    const inputRef= useRef(null)
+    const [text, setText] = useState('')
+    const [editText, setEditText] = useState('')
+    const [editIndex, setEditIndex] = useState(null)
+
+    const onAdd = () => {
+        if(!text) alert('Fill the input');
+        handleAdd(text);
+        inputRef.current.value = '';
+    }
 
     return (
         <div>
-            <button onClick={handleAdd}>Add new task</button>
+            <input ref={inputRef} onChange={e => setText(e.target.value)}/>
+            <button onClick={onAdd}>Add new item</button>
             <ul>
-                {list.map(item => (
-                    <li key={item}>{item}</li>
+                {list.map((item, index) => (
+                    <div>
+                        {editIndex === index ? <input value={editText} onChange={e => setEditText(e.target.value)}/> : <li key={item.id}> {item.name}</li>}
+
+                        {editIndex === index ? 
+                            <button onClick={() =>{
+                                if(!editText) alert('Fill the input')
+                                else {
+                                    handleEdit(index, editText)
+                                    setEditIndex(null);
+                                }
+                                
+                            }}>
+                                save
+                            </button> 
+                            : <button onClick={() => {
+                                setEditIndex(index)
+                                setEditText(item.name)
+                                }}
+                                >
+                                    edit
+                                </button>
+                                }
+                        
+                    
+                    </div>
+
                 ))}
             </ul>
         </div>
     )
-}
+})
